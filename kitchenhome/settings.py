858e18270c64eb4156d328d99d5dc428c47405d9
Ali Aji <ali.a.aji108@gmail.com>
2022-12-06 18:55:23 +0300

    [KTH-6]: 1) Install twilio. 		 2) Create function for sending code.

--- kitchenhome/settings.py
+++ kitchenhome/settings.py
@@ -16,7 +16,6 @@ import datetime
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = "django-insecure-0&t^g-8b5)&y&c&v&m6_)ik8ug@dwm^_9y@!#201q9h_2)ok-9
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
@@ -81,7 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "kitchenhome.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -91,7 +88,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -111,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -125,7 +120,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -137,3 +131,7 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
+
+TWILIO_AUTH_TOKEN = "2094f82b886c5705960f584805f2833b"
+TWILIO_ACCOUNT_SID = "AC61e28653b1fee903bc846d8c09ba4473"
+TWILIO_MESSAGING_SERVICE_SID = "MGb8c95834c55bc80ca1704211fee337c0"
