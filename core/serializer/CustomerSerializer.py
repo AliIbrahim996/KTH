3790ab02b54c217f91e1cb95bd609932965c62f3
Ali Ibrahim <eng.aliibrahim.96@outlook.com>
2023-01-16 20:55:55 +0300

    [KTH-45]: Minor fix, add missing packages.

--- core/serializer/CustomerSerializer.py
+++ core/serializer/CustomerSerializer.py
@@ -4,12 +4,6 @@ from phonenumber_field.serializerfields import PhoneNumberField
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-    full_name = serializers.CharField(source='user.full_name')
-    username = serializers.CharField(source='user.username')
-    phone_number = PhoneNumberField(source='user.phone_number')
-    email = serializers.EmailField(source='user.email')
-    profile_img = serializers.ImageField(source='user.profile_img')
-
     class Meta:
         model = User
         fields = [
